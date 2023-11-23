70193219233b7b1656fd2a40e1879e4a4cc14c91
THETOUCH <super.ilgiznex@gmail.com>
2023-11-23 18:44:24 +0600

    123

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,20 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
+    private static volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+
+    }
+     public static ThreadSafeSingleton getInstance() {
+        if (instance == null) {
+            synchronized (ThreadSafeSingleton.class) {
+                if (instance == null) {
+                    instance = new ThreadSafeSingleton();
+                }
+            }
+        }
+        return instance;
+     }
     // Write your code here!
 }
